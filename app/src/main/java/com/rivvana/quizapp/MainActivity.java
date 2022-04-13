f66ed4e9c7052c0541ea2f643f6bf90017b3d641
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-04-13 13:49:49 +0700

    a

--- app/src/main/java/com/rivvana/quizapp/MainActivity.java
+++ app/src/main/java/com/rivvana/quizapp/MainActivity.java
@@ -3,12 +3,66 @@ package com.rivvana.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView tvQuestion, tvScore, tvTimer;
+    private RadioGroup radioGroup;
+    private RadioButton rb1, rb2;
+    private Button next;
+
+    int totalQuestion;
+    int qCounter = 0;
+    private QuestionModel currentQuestion;
+
+    private List<QuestionModel> questionModelList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        questionModelList = new ArrayList<>();
+        tvQuestion = findViewById(R.id.question);
+        tvScore = findViewById(R.id.score);
+        tvTimer = findViewById(R.id.textTimer);
+
+        radioGroup = findViewById(R.id.rbGrup);
+        rb1 = findViewById(R.id.opsi1);
+        rb2 = findViewById(R.id.opsi2);
+        next = findViewById(R.id.buttonPilih);
+
+        addQuestion();
+        totalQuestion = questionModelList.size();
+        showNextQuestions();
+    }
+
+    private void showNextQuestions() {
+        if (qCounter < totalQuestion){
+            currentQuestion = questionModelList.get(qCounter);
+            tvQuestion.setText(currentQuestion.getQuestion());
+            rb1.setText(currentQuestion.getOption1());
+            rb2.setText(currentQuestion.getOption2());
+
+            qCounter++;
+        }else {
+            finish();
+        }
+
+    }
+
+    private void addQuestion() {
+
+        questionModelList.add(new QuestionModel("A is a correct", "A", "B", 1));
+        questionModelList.add(new QuestionModel("B is a correct", "A", "B", 2));
+
     }
 }
--- app/src/main/java/com/rivvana/quizapp/QuestionModel.java
+++ app/src/main/java/com/rivvana/quizapp/QuestionModel.java
@@ -2,5 +2,44 @@ package com.rivvana.quizapp;
 
 public class QuestionModel {
     private String question, option1, option2;
+    private int correctAnsNo;
 
+    public QuestionModel(String question, String option1, String option2, int correctAnsNo) {
+        this.question = question;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.correctAnsNo = correctAnsNo;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getOption1() {
+        return option1;
+    }
+
+    public void setOption1(String option1) {
+        this.option1 = option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+    public void setOption2(String option2) {
+        this.option2 = option2;
+    }
+
+    public int getCorrectAnsNo() {
+        return correctAnsNo;
+    }
+
+    public void setCorrectAnsNo(int correctAnsNo) {
+        this.correctAnsNo = correctAnsNo;
+    }
 }
